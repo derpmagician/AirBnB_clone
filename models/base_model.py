8044a5e4a9dcdb42fbac3173596552c4ec35d831
derpmagician <renzin@gmail.com>
2021-06-24 01:13:01 -0500

    id and time

--- models/base_model.py
+++ models/base_model.py
@@ -1,15 +1,14 @@
 #!/usr/bin/python3
 """Entry point for BaseModel"""
 import uuid
+from datetime import datetime
 import models
 
 
 class BaseModel:
     """Class for BaseModel.
-    Instances of `BaseModel` autoupdate `updated_at` after
-    any change through an overriden `__setattr__` method.
+    Instances of BaseModel
     """
-    __str_fmt = "[{}] ({}) {}"
 
     def __init__(self, *args, **kwargs):
         """Instantiation for BaseModel.
@@ -17,5 +16,20 @@ class BaseModel:
             *args: arguments.
             **kwargs: keyworded arguments.
         """
-		pass
+		time_attrs = ('created_at', 'updated_at')
+        if kwargs:
+            for k, v in kwargs.items():
+                if k == "__class__":
+                    continue
+                if k in time_attrs:
+                    time_val = datetime.strptime(v, "%Y-%m-%dT%H:%M:%S.%f")
+                    setattr(self, k, time_val)
+                else:
+                    setattr(self, k, v)
+        else:
+            current_time = datetime.now()
+            self.id = str(uuid.uuid4())
+            for a in time_attrs:
+                setattr(self, a, current_time)
+            models.storage.new(self)
 
