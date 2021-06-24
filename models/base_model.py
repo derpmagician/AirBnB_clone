d90ded5498731b8744a5a5231d5984c04e22e399
derpmagician <renzin@gmail.com>
2021-06-24 11:58:17 -0500

    adding storage function

--- models/base_model.py
+++ models/base_model.py
@@ -3,8 +3,7 @@
 import uuid
 from datetime import datetime
 from models import storage
-dformat = "%Y-%m-%dT%H:%M:%S.%f"
-
+format = '%Y-%m-%dT%H:%M:%S.%f'
 
 class BaseModel:
     """Class for BaseModel"""
