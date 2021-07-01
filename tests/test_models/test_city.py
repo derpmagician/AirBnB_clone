4aaf27a8b27f088ea2a24e0b4b4bdc000f8ab801
derpmagician <renzin@gmail.com>
2021-06-30 19:52:49 -0500

    adding tests

--- tests/test_models/test_city.py
+++ tests/test_models/test_city.py
@@ -1,57 +1,138 @@
 #!/usr/bin/python3
-"""test city"""
+"""Test User Class - Comproving expectect outputs and documentation
+"""
+
+from datetime import datetime
+import models
+import pep8
+import inspect
 import unittest
-import os
-from models.city import City
-from models.base_model import BaseModel
-from models.engine.file_storage import FileStorage
+from unittest import mock
+import time
+
+City = models.city.City
+mod_doc = models.city.__doc__
 
 
-class TestCity(unittest.TestCase):
-    """test city class"""
+class TestDocs(unittest.TestCase):
+    """Test documentation and style"""
+    @classmethod
+    def setUpClass(self):
+        """Setup for dosctring"""
+        user_i = inspect.getmembers(City, inspect.isfunction)
 
-    def setUp(self):
-        """Sets up test methods."""
-        FileStorage._FileStorage__file_path = "test_json"
-        self.city = City()
-        self.city.name = "Bogota"
-        self.city.save()
+    def testing_pep8(self):
+        """Testing that models_user.py passes pep8 """
 
-    def test_docstring_City(self):
-        """checking docstrings"""
-        self.assertIsNotNone(City.__doc__)
+    def test_pep8_conformance_user(self):
+        """testing pep8 in city.py"""
+        pep8s = pep8.StyleGuide(quiet=True)
+        result = pep8s.check_files(['models/city.py'])
+        self.assertEqual(result.total_errors, 0,
+                         "Found code style errors (and warnings).")
 
-    def test_attributes_City(self):
-        """chekcing if City have attributes"""
-        self.assertTrue('id' in self.city.__dict__)
-        self.assertTrue('created_at' in self.city.__dict__)
-        self.assertTrue('updated_at' in self.city.__dict__)
-        self.assertTrue('id' in self.city.__dict__)
-        self.assertTrue('name' in self.city.__dict__)
-        self.assertEqual(hasattr(self.city, "name"), True)
+    def test_module_docstring(self):
+        """Test for the existence of module docstring"""
+        self.assertIsNot(mod_doc, None,
+                         "base_model.py needs a docstring")
+        self.assertTrue(len(mod_doc) > 1,
+                        "base_model.py needs a docstring")
 
-    def test_subclass_City(self):
-        """test if City is subclass of Basemodel"""
-        self.assertTrue(issubclass(self.city.__class__, BaseModel), True)
-        self.assertIsInstance(self.city, City)
-
-    def test_type_City(self):
-        """test attribute type for City"""
-        self.assertEqual(type(self.city.name), str)
-        self.assertEqual(type(self.city.state_id), str)
-
-    def test_save_City(self):
-        """test if the save works"""
-        self.city.save()
-        self.assertNotEqual(self.city.created_at, self.city.updated_at)
-
-    def test_to_dict_City(self):
-        """test if dictionary works"""
-        self.assertEqual('to_dict' in dir(self.city), True)
-
-    def tearDown(self):
-        os.remove(FileStorage._FileStorage__file_path)
+    def test_dosctring(self):
+        """Testing documentation"""
+        self.assertIsNot(mod_doc, None,
+                         "base_model.py needs a doctring")
+        self.assertTrue(len(mod_doc) > 1,
+                        "base_model.py needs a docstring")
 
 
-if __name__ == "__main__":
-    unittest.main()
+class TestBaseModel(unittest.TestCase):
+    """testing BaseModel Class"""
+    @mock.patch('models.city')
+    def test_instances(self, mock_storage):
+        """Testing that object is correctly created"""
+        instance = City()
+        self.assertIs(type(instance), City)
+        instance.name = "Holbies foravaaaa"
+        instance.state_id = "111-222"
+
+        expectec_attrs_types = {
+            "id": str,
+            "created_at": datetime,
+            "updated_at": datetime,
+            "state_id": str,
+            "name": str
+        }
+        # testing types and attr names
+        for attr, types in expectec_attrs_types.items():
+            with self.subTest(attr=attr, typ=types):
+                self.assertIn(attr, instance.__dict__)
+                self.assertIs(type(instance.__dict__[attr]), types)
+        self.assertEqual(instance.name, "Holbies foravaaaa")
+        self.assertEqual(instance.state_id, "111-222")
+
+    def test_datetime(self):
+        """testing correct datetime assignation
+        correct assignation of created_at and updated_at"""
+        created_at = datetime.now()
+        instance1 = City()
+        updated_at = datetime.now()
+        self.assertEqual(created_at <= instance1.created_at
+                         <= updated_at, True)
+        time.sleep(1)
+        created_at = datetime.now()
+        instance2 = City()
+        updated_at = datetime.now()
+        self.assertTrue(created_at <= instance2.created_at <= updated_at, True)
+        self.assertEqual(instance1.created_at, instance1.created_at)
+        self.assertEqual(instance2.updated_at, instance2.updated_at)
+        self.assertNotEqual(instance1.created_at, instance2.created_at)
+        self.assertNotEqual(instance1.updated_at, instance2.updated_at)
+
+    def test_uuid(self):
+        """testing uuid"""
+        instance1 = City()
+        instance2 = City()
+        for instance in [instance1, instance2]:
+            tuuid = instance.id
+            with self.subTest(uuid=tuuid):
+                self.assertIs(type(tuuid), str)
+
+    def test_dictionary(self):
+        """testing to_dict correct funtionality"""
+        """Testing that object is correctly created"""
+        instance3 = City()
+        self.assertIs(type(instance3), City)
+        instance3.name = "Holbies foravaaaa"
+        instance3.state_id = "111-222"
+        new_inst = instance3.to_dict()
+        expectec_attrs = ["id",
+                          "created_at",
+                          "updated_at",
+                          "name",
+                          "state_id",
+                          "__class__"]
+        self.assertCountEqual(new_inst.keys(), expectec_attrs)
+        self.assertEqual(new_inst['__class__'], 'City')
+        self.assertEqual(new_inst['name'], 'Holbies foravaaaa')
+        self.assertEqual(new_inst['state_id'], '111-222')
+
+    def test_str_method(self):
+        """testing str method, checking output"""
+        instance4 = City()
+        strr = "[City] ({}) {}".format(instance4.id, instance4.__dict__)
+        self.assertEqual(strr, str(instance4))
+
+    @mock.patch('models.storage')
+    def test_save_method(self, mock_storage):
+        """test save method and if it updates
+        "updated_at" calling storage.save"""
+        instance4 = City()
+        created_at = instance4.created_at
+        updated_at = instance4.updated_at
+        instance4.save()
+        new_created_at = instance4.created_at
+        new_updated_at = instance4.updated_at
+        self.assertNotEqual(updated_at, new_updated_at)
+        self.assertEqual(created_at, new_created_at)
+        self.assertTrue(mock_storage.save.called)
